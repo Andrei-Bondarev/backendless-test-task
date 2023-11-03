f1e99810c9b1736cacd4225c67a77e38aea7c44e
Andrei Bondarev <Andrei14bondarev@gmail.com>
2023-11-03 05:44:49 +0200

    changed public path in wp config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "build.js",
-		publicPath: "/",
+		publicPath: "/backendless-test-task/",
 	},
 	module: {
 		rules: [
